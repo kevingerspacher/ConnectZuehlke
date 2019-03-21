29db0f7c8de155a2bc9346414964454600cb287a
Kevin Gerspacher <kevin.gerspacher@zuehlke.com>
2019-03-21 18:46:43 +0100

    Added function to display visible employees

--- frontend/src/app/employee-table/employee-table.component.ts
+++ frontend/src/app/employee-table/employee-table.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {EmployeeService} from '../employee.service';
 import {Employee} from '../domain/Employee';
 import {Router} from "@angular/router";
-import {MatPaginator, MatTableDataSource} from "@angular/material";
+import {MatPaginator, MatTableDataSource, PageEvent} from "@angular/material";
 
 
 @Component({
@@ -29,13 +29,25 @@ export class EmployeeTableComponent implements OnInit {
     this.router.navigate([`/employee/${employee.code}`]);
   }
 
+  private getVisibleEmployees()
+  {
+    this.datasource.paginator.page.subscribe((pageEvent: PageEvent) => {
+      const startIndex = pageEvent.pageIndex * pageEvent.pageSize;
+      const endIndex = startIndex + pageEvent.pageSize;
+      const itemsShowed = this.datasource.filteredData.slice(startIndex, endIndex);
+      console.log(itemsShowed);
+    });
+  }
+
   private getEmployees() {
     this.employeeService
       .getAllEmployees().
       subscribe(employees => {
       this.datasource = new MatTableDataSource(employees);
       this.datasource.paginator = this.paginator;
+      this.getVisibleEmployees();
     });
+
   }
 
   applyFilter(filterValue: string) {
